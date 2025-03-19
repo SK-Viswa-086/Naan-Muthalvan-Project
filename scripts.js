3105a87ae4357e80d7050a74821073fb1d299db1
Viswa S <23051711802111086@vcsm.ac.in>
2025-03-20 00:50:24 +0900

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// Wait for the DOM to load
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('section');
+    sections.forEach(section => {
+        section.classList.add('fade-in');
+    });
+});
